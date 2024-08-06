ff50d1645de2670cae10335528a4bca891f855aa
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-06 15:45:21 +0900

    User 관련 도메인에 `name` 필드 추가

--- our_favorite_space/users/domain/user.py
+++ our_favorite_space/users/domain/user.py
@@ -6,6 +6,7 @@ from our_favorite_space.users.vo.type import EventTime, UserType
 @dataclass
 class User:
     id: int
+    name: str
     type: UserType
     company: str
     event_times: list[EventTime]
