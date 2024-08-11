da9259f8d624259dbc6434b56d54295f0079aaab
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-11 19:38:04 +0900

    Space Domain 작성 및 유스케이스 Return Type 지정

--- our_favorite_space/space/usecase/manage.py
+++ our_favorite_space/space/usecase/manage.py
@@ -1,21 +1,23 @@
 from abc import ABC, abstractmethod
 
+from our_favorite_space.space.domain.space import Space
+
 
 class CreateUseCase(ABC):
     @abstractmethod
-    def create(self) -> None:
+    def create(self) -> Space:
         pass
 
 
 class GetInfoUseCase(ABC):
     @abstractmethod
-    def get_info(self, id: int) -> None:
+    def get_info(self, id: int) -> Space:
         pass
 
 
 class ModifyUseCase(ABC):
     @abstractmethod
-    def modify(self, id: int) -> None:
+    def modify(self, id: int) -> Space:
         pass
 
 
---
+++ our_favorite_space/space/vo/type.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SpaceType:
+    RESTAURANT = 1
+    RECREATION = 2
