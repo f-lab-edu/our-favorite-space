9eaefb996b383e907d48cc77620ac8ecf7d1f23c
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-12 16:49:26 +0900

    User Domain의 인바운드/아웃바운드 포트 작성

---
+++ our_favorite_space/users/ports/inbound_port.py
@@ -0,0 +1,69 @@
+from our_favorite_space.company.ports.outbound_port import LoadCompanyOutboundPort
+from our_favorite_space.users.domain.user import User
+from our_favorite_space.users.ports.outbound_port import (
+    LoadUserOutboundPort,
+    SaveUserOutboundPort,
+)
+from our_favorite_space.users.usecase.manage import (
+    CreateUseCase,
+    DeleteUseCase,
+    GetInfoUseCase,
+    ModifyUseCase,
+)
+from our_favorite_space.users.vo.type import UserType
+
+
+class UserCreateInboundPort(CreateUseCase):
+    def __init__(
+        self,
+        save_user_port: SaveUserOutboundPort,
+        load_company_port: LoadCompanyOutboundPort,
+    ):
+        self.load_company_port = load_company_port
+        self.save_user_port = save_user_port
+
+    def create(self, name: str, company_id: int) -> User:
+        company = self.load_company_port(company_id)
+        user = User(name=name, type=UserType.NORMAL, company=company)
+        return self.save_user_port.save(user)
+
+    def create_admin(self, name: str, company_id: int) -> User:
+        company = self.load_company_port(company_id)
+        user = User(name=name, type=UserType.COMPANY_ADMIN, company=company)
+        return self.save_user_port.save(user)
+
+
+class UserGetInfoInboundPort(GetInfoUseCase):
+    def __init__(self, load_user_port: LoadUserOutboundPort):
+        self.load_user_port = load_user_port
+
+    def get_info(self, id: int) -> User:
+        return self.load_user_port.get(id)
+
+
+class UserModifyInboundPort(ModifyUseCase):
+    def __init__(
+        self,
+        load_user_port: LoadUserOutboundPort,
+        save_user_port: SaveUserOutboundPort,
+    ):
+        self.load_user_port = load_user_port
+        self.save_user_port = save_user_port
+
+    def modify(self, id: int, **data) -> User:
+        user = self.load_user_port.get(id)
+
+        for key, value in data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+
+        return self.save_user_port.save(user)
+
+
+class UserDeleteInboundPort(DeleteUseCase):
+    def __init__(self, load_user_port: LoadUserOutboundPort):
+        self.load_user_port = load_user_port
+
+    def delete(self, id: int) -> None:
+        user = self.load_user_port.get(id)
+        self.load_user_port.delete(user)
