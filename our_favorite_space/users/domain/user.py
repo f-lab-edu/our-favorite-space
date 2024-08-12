a89d79178e821a23c5f4d30745d814ed0f919c4a
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-12 17:11:54 +0900

    User의 Event Time들을 Dict로 관리하도록 수정

--- our_favorite_space/users/domain/user.py
+++ our_favorite_space/users/domain/user.py
@@ -10,4 +10,4 @@ class User:
     name: str
     type: UserType
     company: Company
-    event_times: list[EventTime] = field(default_factory=list)
+    event_times: dict[str, EventTime] = field(default_factory=dict)
