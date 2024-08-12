065d105525b677a88a64c77ac043199c5c4cbf0f
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-12 23:23:50 +0900

    App 추가

--- our_favorite_space/our_favorite_space/settings.py
+++ our_favorite_space/our_favorite_space/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "company",
+    "message",
+    "rating",
+    "space",
+    "users",
     "rest_framework",
 ]
 
