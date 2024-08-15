9674935b448ea32ee40042fd143d7ac63dd4ef23
Dongyeon1201 <dongyeon1201@gmail.com>
2024-08-15 15:14:11 +0900

    평가(Rating) 도메인에서 장소에 대한 정보를 저장하도록 속성 설정

--- our_favorite_space/rating/domain/rating.py
+++ our_favorite_space/rating/domain/rating.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
 from rating.vo.comment import RecreationRatingData, RestaurantRatingData
+from space.domain.space import Space
 from users.domain.user import User
 
 
 @dataclass
 class Rating:
     user: User
+    space: Space
     data: RestaurantRatingData | RecreationRatingData
     comment: str
